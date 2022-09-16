570f286040932365c23aa8f954506b223713acc3
Ozan Can Kösemez <39047473+ozancank@users.noreply.github.com>
2022-09-16 15:22:05 +0300

    Update SecurityServiceRegistration.cs

--- src/corePackages/Core.Security/SecurityServiceRegistration.cs
+++ src/corePackages/Core.Security/SecurityServiceRegistration.cs
@@ -1,5 +1,18 @@
-﻿namespace Core.Security;
+using Core.Security.EmailAuthenticator;
+using Core.Security.JWT;
+using Core.Security.OtpAuthenticator;
+using Core.Security.OtpAuthenticator.OtpNet;
+using Microsoft.Extensions.DependencyInjection;
 
-public class SecurityServiceRegistration
+namespace Core.Security;
+
+public static class SecurityServiceRegistration
 {
+    public static IServiceCollection AddSecurityServices(this IServiceCollection services)
+    {
+        services.AddScoped<ITokenHelper, JwtHelper>();
+        services.AddScoped<IEmailAuthenticatorHelper, EmailAuthenticatorHelper>();
+        services.AddScoped<IOtpAuthenticatorHelper, OtpNetOtpAuthenticatorHelper>();
+        return services;
+    }
 }
